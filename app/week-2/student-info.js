2193b19e25553b31a942aeeca182f7bcfa33cf55
LTG-CB <curtis.borson@edu.sait.ca>
2024-09-19 15:03:55 -0600

    Week-2
    create week-2 page
    remove original code to root page
    add link to week-2 page on root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Curtis</h1>
+      <a href="https://github.com/LTG-CB/cprg306-assignments.git" target="_blank">
+        GitHub Repository
+      </a>
+    </div>
+  );
+};
+
+export default StudentInfo;
